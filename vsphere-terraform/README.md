fd116c3d02dcd2d17ff4ebaeea1a14874b066c4d
Sean Crosby <crosbyse@byui.edu>
2025-07-25 15:02:58 -0600

    linux box fixes

--- vsphere-terraform/README.md
+++ vsphere-terraform/README.md
@@ -19,6 +19,8 @@ The domain names are:
 * `los-git.cse.byui.edu` (157.201.19.93)
 * `los-matrix.cse.byui.edu` (157.201.19.92)
 
+These are all on the I-CIT-PUBLIC-19-K8S network (dvportgroup-2552).
+
 ## Credentials
 
 The following credentials are used on the system:
@@ -26,3 +28,13 @@ The following credentials are used on the system:
 * vSphere - BYUI Creds (must be connected to VPN)
 * Debian Templates
    * Sean Crosby has an account
+
+## Template Details
+
+### Debian-12.11.0-Net
+
+Based on the debian-12.11.0 netinstall ISO.  After installing the ISO, the following additions were made:
+1. Configure a static IP (the public IP) in `/etc/network/interfaces`.  Note that this IP address will have to be manually updated since we didn't get it working with terraform (due to a vmware tools limitation?)
+2. Setup the sources.list for apt
+3. Install the following packages with apt: vim, open-vm-tools, net-tools
+4. Configure resolve.conf (for 8.8.8.8)
--- vsphere-terraform/linux_vm.tf
+++ vsphere-terraform/linux_vm.tf
@@ -4,15 +4,15 @@ data "vsphere_virtual_machine" "linux_template" {
 }
 
 resource "vsphere_virtual_machine" "linux_vm" {
-  name             = "jump"
+  name             = "linux"
   folder           = var.folder_name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus   = 8
   memory     = 16384
-  guest_id   = data.vsphere_virtual_machine.jump_template.guest_id
-  firmware   = data.vsphere_virtual_machine.jump_template.firmware
+  guest_id   = data.vsphere_virtual_machine.linux_template.guest_id
+  firmware   = data.vsphere_virtual_machine.linux_template.firmware
   cdrom {
     client_device = true
   }
@@ -24,13 +24,13 @@ resource "vsphere_virtual_machine" "linux_vm" {
 
   disk {
     label            = "disk0"
-    size             = data.vsphere_virtual_machine.jump_template.disks.0.size
+    size             = data.vsphere_virtual_machine.linux_template.disks.0.size
     eagerly_scrub    = false
     thin_provisioned = false
   }
 
   clone {
-    template_uuid = data.vsphere_virtual_machine.jump_template.id
+    template_uuid = data.vsphere_virtual_machine.linux_template.id
   }
 }
 
